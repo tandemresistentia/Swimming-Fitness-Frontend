f82346a6807185cb1ca22d702bfca785b4d38808
Luis <deathghoul41@gmail.com>
2023-06-22 01:18:34 +0200

    final

--- src/components/config.js
+++ src/components/config.js
@@ -1,4 +1,4 @@
 
-const BASE_URL1 = 'https://swimming-fitness-backend-production.up.railway.app';
-const BASE_URL = 'http://127.0.0.1:8000'
+const BASE_URL = 'https://swimming-fitness-backend-production.up.railway.app';
+const BASE_URL1 = 'http://127.0.0.1:8000'
 export default BASE_URL;
